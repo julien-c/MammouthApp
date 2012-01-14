73a6a13834916d70a0e36b3e0dbe3e02dc3ca14a
Julien Chaumond <chaumond@gmail.com>
2012-01-14 21:27:00 +0100

    Initial Commit

---
+++ MammouthAppTests/MammouthAppTests.h
@@ -0,0 +1,13 @@
+//
+//  MammouthAppTests.h
+//  MammouthAppTests
+//
+//  Created by Julien Chaumond on 14/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MammouthAppTests : SenTestCase
+
+@end
---
+++ MammouthAppTests/MammouthAppTests.m
@@ -0,0 +1,32 @@
+//
+//  MammouthAppTests.m
+//  MammouthAppTests
+//
+//  Created by Julien Chaumond on 14/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MammouthAppTests.h"
+
+@implementation MammouthAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MammouthAppTests");
+}
+
+@end
